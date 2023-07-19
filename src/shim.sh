1ee8c7ba33d39357f157f77bd2941e2d71f1cce3
Aaron <aaron.z@covergenius.com>
2023-07-19 11:34:59 +1000

    refactor templates

--- src/shim.sh
+++ src/shim.sh
@@ -17,12 +17,18 @@ function _ensure_version_installed {
 
 function _find_executable {
   local VERSION
+  local REAL_LINK
+  local LINK_DIR
   if [ -f ".poetry-version" ]; then
     VERSION=$(head -n 1 .poetry-version)
     _ensure_version_installed "${VERSION}"
+    export VIRTUAL_ENV=${BASE_DIR}/${VERSION}
     POETRY="${BASE_DIR}/${VERSION}/bin/poetry"
   else
     POETRY="${BASE_DIR}/global"
+    REAL_LINK=$(readlink "${POETRY}")
+    LINK_DIR=$(dirname "${REAL_LINK}")
+    export VIRTUAL_ENV=${LINK_DIR}
     if [ ! -f "${POETRY}" ]; then
       echo "Global poetry version has not been set" > /dev/stderr
       exit 1
